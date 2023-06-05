40d1f1b99b8290b57963485291487a03a218dbb6
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-06-05 18:14:35 +0300

    App

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import RegistrationScreens from "./Screens/RegistrationScreen.jsx";
+import LoginScreen from "./Screens/LoginScreen.jsx";
+import Home from "./Screens/Home.jsx";
+
+// import Login from "./screens/Login";
+// import Register from "./screens/Register";
+// import Home from "./screens/Home";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { useFonts } from "expo-font";
 
-import RegistrationScreens from "./Screens/RegistrationScreen.jsx";
-import LoginScreen from "./Screens/LoginScreen.jsx";
-import PostsScreen from "./Screens/PostsScreen.jsx";
-
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto: require("./assets/fonts/Roboto-Regular.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  return (
-    <View style={styles.container}>
-      <RegistrationScreens />
-      {/* <LoginScreen /> */}
-      {/* <PostsScreen /> */}
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const MainStack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -33,3 +24,36 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <View style={styles.container}>
+          <MainStack.Screen
+            name="Registration"
+            component={RegistrationScreens}
+          />
+          <MainStack.Screen name="Login" component={LoginScreen} />
+          <MainStack.Screen name="Home" component={Home} />
+
+          {/* <View style={styles.container}>
+          <RegistrationScreens />
+          <LoginScreen />
+          <StatusBar style="auto" />
+        </View> */}
+          <StatusBar style="auto" />
+        </View>
+      </MainStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ Screens/CreatePostsScreen.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: 17,
+    lineHeight: 22,
+    letterSpacing: -0.4,
+    textAlign: "center",
+    color: "#212121",
+  },
+});
+
+const CreatePostsScreenScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>CreatePostsScreenScreen</Text>
+    </View>
+  );
+};
+
+export default CreatePostsScreenScreen;
